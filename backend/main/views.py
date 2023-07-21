de7a54b7de5c4da0501c28c3e9e6967083b7f2e5
dridi14 <dridi.mohamedamine@hxperience.com>
2023-07-21 12:38:45 +0200

    test

--- backend/main/views.py
+++ backend/main/views.py
@@ -79,7 +79,7 @@ class RoomSensorDataView(APIView):
         room = get_object_or_404(Room, id=room_id)
         sensor = get_object_or_404(Sensor, sensor_id=sensor_id, room=room, id=id)
 
-        if interval is None:
+        if interval is None or interval == '':
             sensor_data = sensor.sensordata_set.all().order_by('-created_at')[:50]
         elif interval == 'hour':
             sensor_data = sensor.sensordata_set.all().order_by('-created_at')[:20]
